7af2e01910cfe8f4d8dc9921372173e642d6d9a9
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-04-16 17:28:01 -0300

    Initial commit and spotify basic style

---
+++ spotify-player/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+
+import { GlobalStyle } from "./styles/global";
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <GlobalStyle />
+                <h1>Spotify</h1>
+            </Fragment>
+        );
+    }
+}
+
+export default App;
