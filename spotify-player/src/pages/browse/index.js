aaf009cb36dbd3d49e6bee91cd1da7f0f822ffe5
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-04-29 22:28:56 -0300

    Router and initial album design

---
+++ spotify-player/src/pages/browse/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Container, Title, List, Playlist } from "./styles";
+
+const Browse = () => (
+    <Container>
+        <Title>Home</Title>
+
+        <List>
+            <Playlist href="">
+                <img
+                    src="https://pt.wikipedia.org/wiki/Concrete_and_Gold#/media/File:Foo_Fighters_-_Concrete_and_Gold_-_2017.jpg"
+                    alt="Playlist"
+                />
+                <strong>Rock dos bons</strong>
+                <p>
+                    Relaxe enquanto você programa ouvindo apenas as melhores do
+                    rock mundial!
+                </p>
+            </Playlist>
+        </List>
+    </Container>
+);
+
+export default Browse;
