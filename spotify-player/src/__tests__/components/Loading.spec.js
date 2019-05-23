8e723f8f490990ce09653e84e7d96dbeadf904e2
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-05-22 21:24:56 -0300

    Added coverage files config

--- spotify-player/src/__tests__/components/Loading.spec.js
+++ spotify-player/src/__tests__/components/Loading.spec.js
@@ -1,16 +1,19 @@
 import React from 'react';
+// import createStore, { mockStore } from 'redux-mock-store';
 import { mount } from 'enzyme';
 
 import LoadingIcon from '../../assets/images/loading.svg';
 
 import Loading from '../../components/Loading';
 
-it('Should render the img component', () => {
-  const wrapper = mount(<Loading />);
-  expect(wrapper.find('img').exists()).toBe(true);
-});
+describe('Loading Component', () => {
+  it('Should render the img component', () => {
+    const wrapper = mount(<Loading />);
+    expect(wrapper.find('img').exists()).toBe(true);
+  });
 
-it('Should render the correct image', () => {
-  const wrapper = mount(<Loading />);
-  expect(wrapper.find('img').prop('src')).toBe(LoadingIcon);
+  it('Should render the correct image', () => {
+    const wrapper = mount(<Loading />);
+    expect(wrapper.find('img').prop('src')).toBe(LoadingIcon);
+  });
 });
