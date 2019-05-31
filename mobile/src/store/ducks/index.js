58fc99f3a22cb228bd94da5a8819ec1a04e86207
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-05-31 00:12:37 -0300

    Podcasts saga and get podcasts list from json api

--- mobile/src/store/ducks/index.js
+++ mobile/src/store/ducks/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { reducer as podcasts } from './podscats';
+import { reducer as podcasts } from './podcasts';
 
 const reducers = combineReducers({
   podcasts,
