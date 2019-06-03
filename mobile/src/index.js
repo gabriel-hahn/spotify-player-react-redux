4ff8363e494435db89809279bd2b72c879a3dd4d
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-06-02 23:44:27 -0300

    Player component style

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -7,9 +7,12 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Routes from '~/routes';
 
+import Player from '~/components/Player';
+
 const App = () => (
   <Provider store={store}>
     <Routes />
+    <Player />
   </Provider>
 );
 
