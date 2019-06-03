c7462cd9181094b42cabf91b53d55e8c26da7aa8
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-06-02 23:08:37 -0300

    Podcast page style

--- mobile/src/pages/Podcast/index.js
+++ mobile/src/pages/Podcast/index.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container,
+  EpisodeList,
+  PodcastDetails,
+  Background,
+  Cover,
+  BackButton,
+  PodcastTitle,
+  PlayButton,
+  PlayButtonText,
+  Episode,
+  Title,
+  Author,
+} from './styles';
 
 export default class Podcast extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  handleBack = () => {
+    const { navigation } = this.props;
+
+    navigation.goBack();
+  };
 
   render() {
+    const { navigation } = this.props;
+    const podcast = navigation.getParam('podcast');
+
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Container>
+        <EpisodeList
+          ListHeaderComponent={() => (
+            <PodcastDetails>
+              <Background source={{ uri: podcast.cover }} blurRadius={5} />
+              <BackButton onPress={this.handleBack}>
+                <Icon name="arrow-back" size={24} color="#fff" />
+              </BackButton>
+
+              <Cover source={{ uri: podcast.cover }} />
+
+              <PodcastTitle>{podcast.title}</PodcastTitle>
+              <PlayButton onPress={() => {}}>
+                <PlayButtonText>Shuffle Play</PlayButtonText>
+              </PlayButton>
+            </PodcastDetails>
+          )}
+          data={podcast.tracks}
+          keyExtractor={episode => String(episode.id)}
+          renderItem={({ item: episode }) => (
+            <Episode>
+              <Title>{episode.title}</Title>
+              <Author>{episode.artist}</Author>
+            </Episode>
+          )}
+        />
+      </Container>
     );
   }
 }
