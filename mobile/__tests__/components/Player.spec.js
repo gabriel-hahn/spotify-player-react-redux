f85522bbfd8db57f9d35a2e28430e83344249b1a
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-06-07 00:31:36 -0300

    Player unit tests

--- mobile/__tests__/components/Player.spec.js
+++ mobile/__tests__/components/Player.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
 import createStore from 'redux-mock-store';
+import ReactTestRenderer from 'react-test-renderer';
 
 import Player from '../../src/components/Player';
 
@@ -10,8 +10,16 @@ const mockStore = createStore();
 const INITIAL_STATE = {
   player: {
     podcast: {
-      tracks: [{ id: 1, artwork: 'https://s3-sa-east-1.amazonaws.com/gonative/cover1.png' }],
+      tracks: [
+        {
+          id: 1,
+          artwork: 'https://s3-sa-east-1.amazonaws.com/gonative/cover1.png',
+          title: 'Welcome to the Jungle',
+          artist: 'Guns n Roses',
+        },
+      ],
     },
+    playing: true,
     current: 1,
   },
 };
@@ -21,7 +29,7 @@ const store = mockStore(INITIAL_STATE);
 let wrapper;
 
 beforeEach(() => {
-  wrapper = shallow(
+  wrapper = ReactTestRenderer.create(
     <Provider store={store}>
       <Player />
     </Provider>,
@@ -29,7 +37,25 @@ beforeEach(() => {
 });
 
 describe('Player component', () => {
-  it('Smoke tests', () => {
-    expect(wrapper.exists());
+  describe('Smoke tests', () => {
+    it('Should render correctly', () => {
+      expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('Component structure', () => {
+    it('Should have the correct uri artwork', () => {
+      expect(wrapper.root.findByType('Image').props.source.uri).toEqual(
+        'https://s3-sa-east-1.amazonaws.com/gonative/cover1.png',
+      );
+    });
+
+    it('Should have the correct title of music', () => {
+      expect(wrapper.root.findAllByType('Text')[0].props.children).toEqual('Welcome to the Jungle');
+    });
+
+    it('Should have the correct artist of music below title', () => {
+      expect(wrapper.root.findAllByType('Text')[1].props.children).toEqual('Guns n Roses');
+    });
   });
 });
